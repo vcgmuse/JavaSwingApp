11c3fa7885a4698da99cf9d495c2ac40b1a91639
Emmanuel Vega <vcgmuse@gmail.com>
2021-03-09 14:58:21 -0600

    Creating A Java Swing App

---
+++ src/com/vcgmuse/App.java
@@ -0,0 +1,13 @@
+package com.vcgmuse;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class App {
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("Hello World!");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(600,500);
+    frame.setVisible(true);
+  }
+}
